8efd3328a4954d8501e6d799b980271dac6ac9e8
Juniper <86971433+JuneDeveloper@users.noreply.github.com>
2023-08-09 16:45:43 +0100

    Added ReLu/ReLu deriv

--- src/math/math.rs
+++ src/math/math.rs
@@ -13,3 +13,26 @@ pub fn dot_product<T: Into<f32> + Copy>(x: &[T], y: &[T]) -> f32 {
         .map(|(&i, &j)| i.into() * j.into())
         .sum()
 }
+
+/// ReLu function
+pub fn relu(x: f32) -> f32 {
+    if x > 0.0 {
+        x
+    } else {
+        0.0
+    }
+}
+
+/// ReLu derivative
+pub fn relu_derivative(x: &[f32], y: &[f32]) -> f32 {
+    let weighted_sum = x
+        .iter()
+        .zip(y.iter())
+        .map(|(&xi, &yi)| xi * yi)
+        .sum::<f32>();
+    if weighted_sum > 0.0 {
+        1.0
+    } else {
+        0.0
+    }
+}
